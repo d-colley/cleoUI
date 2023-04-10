1c96d8c7999bb4a6c3a0c54ec8931d00f478e600
d-colley <david_colley123@yahoo.com>
2023-04-10 11:19:40 -0500

    base app upload

---
+++ CleoUI/CleoUI/Models/Instrument.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleoUI.Models
+{
+    public class Instrument
+    {
+        [Key]
+        public int InstrumentID { get; set; }
+        [Required]
+        public string InstrumentName { get; set; }
+        public string InstrumentCode { get; set; }
+        public string InstrumentURL { get; set; }
+
+    }
+}
---
+++ CleoUI/CleoUI/Models/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace CleoUI.Models
+{
+    public class Result
+    {
+        public  int InstrumentID { get; set; }
+        public string WeeklyResult { get; set; }
+        public string MonthlyResult { get; set; }
+        public int Price { get; set; }
+        public DateTime When { get; set; }
+    }
+}
